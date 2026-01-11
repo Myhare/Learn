8bc72ad47ea87a3fafd3fe89c08f9b2a4ee581bc
Ming <1940307627@qq.com>
2026-01-11 20:17:27 +0800

    长度最小的子数组

---
+++ src/算法练习/力扣练习/中等难度/长度最小的子数组.java
@@ -0,0 +1,36 @@
+package 算法练习.力扣练习.中等难度;
+
+public class 长度最小的子数组 {
+
+    public static final int INIT_SIZE = 0;
+
+    public static int minSubArrayLen(int target, int[] nums) {
+        int minSize = INIT_SIZE;
+
+        int left = 0, right = 0;
+        int tempSize = 0;
+        while (right < nums.length){
+            tempSize = tempSize + nums[right];
+            while (tempSize >= target && left <= right){
+                int len = right - left + 1;
+                minSize = minSize == INIT_SIZE ? len : Math.min(minSize, len);
+                tempSize = tempSize - nums[left];
+                left++;
+            }
+            right++;
+        }
+
+
+        return minSize;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 1, 2, 4, 3};
+        int[] arr2 = {1,4,4};
+        int[] arr3 = {1,1,1,1,1,1,1,1};
+        System.out.println(minSubArrayLen(7, arr));
+        System.out.println(minSubArrayLen(4, arr2));
+        System.out.println(minSubArrayLen(11, arr3));
+    }
+
+}
